82f6f34dd6cd89ff638815fe82a393ced538898f
jcandalijabl <jcandalija.smx@institutcampalans.net>
2024-04-27 21:00:00 +0200

    27/04/2024 v1

---
+++ func.js
@@ -0,0 +1,17 @@
+
+function OCactive(){
+    return `
+      <style>
+        .audio-player {
+          background: #d75a44;
+          border-radius: 5px;
+          padding: 5px;
+          color: #fff;
+          display: flex;
+          align-items: center;
+          position: relative;
+          margin: 0 0 25px;
+        }
+      </style>
+    `
+}
